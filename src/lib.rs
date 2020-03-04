6fce17abbfb9ff2e69ee07c299652ce1e8d876e5
Dominic Tassio <dominic.tassio@gmail.com>
2020-03-03 22:46:20 -0600

    Paging Implementation

--- src/lib.rs
+++ src/lib.rs
@@ -7,9 +7,12 @@
 
 pub mod gdt;
 pub mod interrupts;
+pub mod memory;
 pub mod serial;
 pub mod vga_buffer;
 
+#[cfg(test)]
+use bootloader::{entry_point, BootInfo};
 use core::panic::PanicInfo;
 
 pub fn init() {
@@ -58,10 +61,12 @@ pub fn test_panic_handler(info: &PanicInfo) -> ! {
     hlt_loop();
 }
 
+#[cfg(test)]
+entry_point!(test_kernel_main);
+
 /// Entry point for `cargo xtest`
 #[cfg(test)]
-#[no_mangle]
-pub extern "C" fn _start() -> ! {
+fn test_kernel_main(_boot_info: &'static BootInfo) -> ! {
     init();
     test_main();
     hlt_loop();
